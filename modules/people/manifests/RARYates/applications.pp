910064c9503eb1c4647be1aa2fab7986f71b2353
Ryan Russell-Yates <rary@na-ad-guest-lwap-ar01.cat.com>
2016-02-25 08:54:53 -0600

    Bug: Swapped Variables to params...

--- modules/people/manifests/RARYates/applications.pp
+++ modules/people/manifests/RARYates/applications.pp
@@ -2,10 +2,10 @@ class people::raryates::applications {
   include iterm2::stable
 
   class {'virtualbox':
-    $version => '5.0.14',
-    $patch_level => '105127',
+    version => '5.0.14',
+    patch_level => '105127',
   }
 
-  class {'vagrant': $version => '1.8.1'}
+  class {'vagrant': version => '1.8.1'}
 
 }
