2d635ceb7b468bc7432762980171cc2fd0b85044
Ryan Russell-Yates <rary@Ryans-MacBook-Pro.local>
2016-02-26 17:04:30 -0600

    Added features

--- modules/people/manifests/raryates/applications.pp
+++ modules/people/manifests/raryates/applications.pp
@@ -7,6 +7,7 @@ class people::raryates::applications {
   include onepassword
   include onepassword::chrome
   include dropbox
+  include zsh
 
   class {'spectacle': version => '1.0.3'}
 
@@ -21,6 +22,16 @@ class people::raryates::applications {
     source   => 'https://www.mediaatelier.com/CheatSheet/download.php',
     provider => compressed_app,
     flavor   => 'zip',
+  }
+
+  package { 'iStat Menus':
+    source   => 'http://s3.amazonaws.com/bjango/files/istatmenus3/istatmenus3.27.zip',
+    provider => compressed_app
+  }
+
+  package { 'CoRD':
+    source   => 'http://iweb.dl.sourceforge.net/project/cord/cord/0.5.7/CoRD_0.5.7.zip',
+    provider => compressed_app,
   }
 
 }
